473b056ed68cb95e44cfc86880b02b0e0222945f
jobchoi <sedcreta12@gmail.com>
2024-09-06 14:36:02 +0900

    footer 추가

--- org.web.Everest/apps/frontend/src/app/app.component.ts
+++ org.web.Everest/apps/frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, HeaderComponent, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
