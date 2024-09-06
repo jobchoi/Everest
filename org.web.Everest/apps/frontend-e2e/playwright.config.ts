4b3067d257336af0ba42849c1f7636de93d21644
jobchoi <sedcreta12@gmail.com>
2024-09-06 13:51:00 +0900

    프로젝트 오타 수정 및 그로인해 npx nx serve 명령어가 안되던 부분 수정

--- org.web.Everest/apps/frontend-e2e/playwright.config.ts
+++ org.web.Everest/apps/frontend-e2e/playwright.config.ts
@@ -25,9 +25,9 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run fontend:serve',
+    command: 'npx nx run frontend:serve',
     url: 'http://localhost:4200',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env['CI'],
     cwd: workspaceRoot,
   },
   projects: [
