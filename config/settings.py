d458ce853ece74c18683be19b512f0bcf1ecef7c
jobchoi <sedcreta12@gmail.com>
2025-11-10 10:51:45 +0900

    DB 수정

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'    :   'django.db.backends.postgresql',
+        'NAME'      :   'django_mvp_db',
+        'USER'      :   'postgres',
+        'PASSWORD'  :   'password',
+        'HOST'      :   'localhost',
+        'PORT'      :   '5432',
     }
 }
 
